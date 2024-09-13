dba2600af7bde57bf2b444df3bbd3c72ca6f4ef3
GokulTR-07 <gokultr1206@gmail.com>
2024-09-13 13:20:43 +0530

    redeploy to ec2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
-
-// Load environment variables from the .env file
-dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env.VITE_APP_NODE_ENV': JSON.stringify(process.env.VITE_APP_NODE_ENV),
-    'process.env.VITE_APP_SERVER_BASE_URL': JSON.stringify(process.env.VITE_APP_SERVER_BASE_URL),
-  },
+  // define: {
+  //   'import.meta.env.VITE_APP_NODE_ENV': JSON.stringify(process.env.VITE_APP_NODE_ENV),
+  //   'import.meta.env.VITE_APP_SERVER_BASE_URL': JSON.stringify(process.env.VITE_APP_SERVER_BASE_URL),
+  // },
 });
